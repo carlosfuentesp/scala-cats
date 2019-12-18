24ea6bb16c221a168925ce447f1001059cf53fd1
Pame Plúas <pamepluas@gmail.com>
2019-12-18 13:14:00 -0500

    Resolves exercise monoid

--- Monoids & Semigroups/Monoids.scala
+++ Monoids & Semigroups/Monoids.scala
@@ -1,22 +1,37 @@
 // 1: Define an abstract class Monoid[A] with combine(x,y) and empty methods
+trait Monoid[A] {
+  def combine(a: A, b: A): A
+  def empty: A
+}
 
 object Monoids {
   // 2: Create a SumIntMonoid
-  // implicit val sumIntMonoid: Monoid[Int] = new Monoid[Int]
+
+  implicit val sumIntMonoid = new Monoid[Int] {
+    def combine(a: Int, b: Int): Int = a + b
+    def empty = 0
+  }
 
   // 3: Create a StringMonoid
+  implicit val stringMonoid = new Monoid[String] {
+    def combine(a: String, b: String): String = s"$a$b"
+    def empty = ""
+  }
 
   // 4: Create a BooleanMonoid with 'or' operation and true as empty
+  implicit val booleanMonoid = new Monoid[Boolean] {
+    def combine(a: Boolean, b: Boolean): Boolean = a || b
+    def empty = true
+  }
 
   def sum[A](list: List[A])(implicit m: Monoid[A]): A =
     if (list.isEmpty) m.empty
     else m.combine(list.head, sum(list.tail))
 
   def main(args: Array[String]): Unit = {
-
     println(sum(List(4, 5)))
-    //println(sum(List("a", "b", "c")))
-    //println(sum(List(true, false)))
-    //println(sum[String](List()))
+    println(sum(List("a", "b", "c")))
+    println(sum(List(true, false)))
+    println(sum[String](List()))
   }
 }
